ed2ef1d4b892256a3eb9504c66a9f0290e7b46b9
imccllr <imccllr@gmail.com>
2023-03-17 15:03:02 -0600

    First commit

---
+++ src/input.py
@@ -0,0 +1,10 @@
+print("Welcome to the greater program");
+name = input("Enter your name: ");
+print("Greetings " + name);     
+
+print("Calculator program");
+firstNumber = input("First number: ");
+secondNumber = input("Second number: ");
+print("String: " + firstNumber + secondNumber);
+print("Float: " + str(float(firstNumber) + float(secondNumber)));
+print("Int: " + str(int(firstNumber) + int(secondNumber)));
---
+++ src/program.py
@@ -0,0 +1,18 @@
+sum = 1 + 2
+product = sum * 2
+print(sum)
+print(product)
+print("Show this in console")
+
+planets_in_solar_system = 8 # int, pluto used to be the 9th planet, but is too small
+distance_to_alpha_centauri = 4.367 # float, lightyears
+can_liftoff = True
+shuttle_landed_on_the_moon = "Apollo 11" #string 
+print(type(distance_to_alpha_centauri))
+
+left_side = 10
+right_side = 5
+print("Dividing 10 by 5: " + str(left_side / right_side))
+print("Multiplying 10 by 5: " + str(left_side * right_side))
+print("Subtracting 10 minus 5: " + str(left_side - right_side))
+print("Adding 10 and 5: " + str(left_side + right_side))
